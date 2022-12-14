f0dcfc92d32a19cea575a20409bfcb33dfbd5688
Kendal Blythe <kendal.blythe@gmail.com>
2022-12-14 18:24:19 -0500

    eslint cleanup

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -5,7 +5,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
-    "next",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "turbo",
