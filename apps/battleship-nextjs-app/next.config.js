55acf50880c70704dd4a68b2889e24b829480da5
Kendal Blythe <kendal.blythe@gmail.com>
2025-12-06 16:35:59 -0500

    support camel case css modules for nextjs

--- apps/battleship-nextjs-app/next.config.js
+++ apps/battleship-nextjs-app/next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  webpack: (config) => {
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes('css-loader'))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = 'camelCase';
+        }
+      });
+
+    return config;
+  },
 };
 
 export default nextConfig;
